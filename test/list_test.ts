189c1410107fb8993d6443dc741c626226ffd5e1
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-01-07 23:19:02 -0500

    Delete cruft

--- test/list_test.ts
+++ test/list_test.ts
@@ -20,15 +20,5 @@ describe('List', () => {
   it('is a list', () => {
     const list: List<number> = new Cons(1, new Cons(2, Nil))
     checkList(list)
-    assert.strictEqual(list.kind, 'cons')
-    assert.strictEqual(list.head, 1)
-    assert.strictEqual(list.tail.kind, 'cons')
-    if (list.tail.kind === 'cons') {
-      assert.strictEqual(list.tail.head, 2)
-      assert.strictEqual(list.tail.tail.kind, 'nil')
-      if (list.tail.tail.kind === 'nil') {
-        assert.strictEqual(list.tail.tail, Nil)
-      }
-    }
   })
 })
