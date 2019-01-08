22a53c1b18cc80b9edd486ae8526692a57a76d62
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-01-08 12:26:43 -0500

    Add exhaustive case check to list test

--- test/list_test.ts
+++ test/list_test.ts
@@ -1,19 +1,36 @@
 import assert from 'assert'
 import { Cons, List, Nil } from '../src/list'
 
-const checkList = (list: List<number>): void => {
-  assert.strictEqual(list.kind, 'cons')
-  if (list.kind === 'cons') {
-    assert.strictEqual(list.head, 1)
-    assert.strictEqual(list.tail.kind, 'cons')
-    if (list.tail.kind === 'cons') {
-      assert.strictEqual(list.tail.head, 2)
-      assert.strictEqual(list.tail.tail.kind, 'nil')
-      if (list.tail.tail.kind === 'nil') {
-        assert.strictEqual(list.tail.tail, Nil)
-      }
-    }
+const checkCons = (list: List<number>, n: number): List<number> => {
+  switch (list.kind) {
+    case 'cons':
+      assert.strictEqual(list.head, n)
+      return list.tail
+    case 'nil':
+      throw new Error('Expected Cons, found Nil')
+    default:
+      const exhaustiveCheck: never = list
+      return exhaustiveCheck
   }
+}
+
+const checkNil = (list: List<number>): void => {
+  switch (list.kind) {
+    case 'cons':
+      throw new Error('Expected Nil, found Cons')
+    case 'nil':
+      assert.strictEqual(list, Nil)
+      break
+    default:
+      const exhaustiveCheck: never = list
+      return exhaustiveCheck
+  }
+}
+
+const checkList = (list: List<number>): void => {
+  const list2 = checkCons(list, 1)
+  const list3 = checkCons(list2, 2)
+  checkNil(list3)
 }
 
 describe('List', () => {
