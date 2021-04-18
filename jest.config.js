bfe35143171a636372862d0b7bca012648a0c879
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-04-18 09:06:07 -0400

    More jest test config
    Add jest test coverage thresholds.
    Lint jest config file.
    Remove old, unused nyc config.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  coverageThreshold: {
+    global: {
+      branches: 88,
+      functions: 68,
+      lines: 85,
+      statements: 85,
+    },
+  },
+}
