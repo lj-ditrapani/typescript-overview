505f698ae2d7052f33da8f3f10fada5f71ac0ebe
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-23 00:47:13 -0400

    Refactor List<A> implementation.
    Leverage BaseList and this type so we don't have to duplicate method
    definition across Nil and Cons.

--- src/list.ts
+++ src/list.ts
@@ -1,55 +1,52 @@
-class NilClass {
-  public readonly kind: 'nil' = 'nil'
-
-  readonly isEmpty = (): boolean => true
-
-  readonly reduce = <B>(zero: B, _f: (acc: B, item: never) => B): B => zero
-
-  readonly reverse = (): List<never> => this
-
-  readonly map = <B>(_unused: (item: never) => B): List<B> => this
-
-  readonly size = (): number => 0
-
-  readonly toString = (): string => 'List( )'
-}
-
-export const nil = new NilClass()
-
-export class Cons<A> {
-  public readonly kind: 'cons' = 'cons'
-
-  constructor(public readonly head: A, public readonly tail: List<A>) {}
-
-  readonly isEmpty = (): boolean => false
-
-  readonly reduce = <B>(zero: B, f: (acc: B, item: A) => B): B => reduce(this, zero, f)
-
-  readonly reverse = (): List<A> =>
-    this.reduce<List<A>>(nil, (acc, item) => new Cons(item, acc))
-
-  readonly map = <B>(f: (item: A) => B): List<B> =>
-    this.reverse().reduce<List<B>>(nil, (acc, item) => new Cons(f(item), acc))
-
-  readonly size = (): number => this.reduce(0, (acc, _unused) => acc + 1)
-
-  readonly toString = (): string =>
-    this.reduce('List( ', (acc, item) => `${acc}${item} `) + ')'
-}
-
-const reduce = <A, B>(list: List<A>, zero: B, f: (acc: B, item: A) => B): B => {
-  let node: List<A> = list
-  let acc = zero
-  while (true) {
-    switch (node.kind) {
-      case 'cons':
-        acc = f(acc, node.head)
-        node = node.tail
-        break
-      case 'nil':
-        return acc
+class BaseList<A> {
+  reduce<B>(this: List<A>, zero: B, f: (acc: B, item: A) => B): B {
+    let node: List<A> = this
+    let acc = zero
+    while (true) {
+      switch (node.kind) {
+        case 'cons':
+          acc = f(acc, node.head)
+          node = node.tail
+          break
+        case 'nil':
+          return acc
+      }
     }
+  }
+
+  reverse(this: List<A>): List<A> {
+    return this.reduce<List<A>>(nil, (acc, item) => new Cons(item, acc))
+  }
+
+  map<B>(this: List<A>, f: (item: A) => B): List<B> {
+    return this.reverse().reduce<List<B>>(nil, (acc, item) => new Cons(f(item), acc))
+  }
+
+  size(this: List<A>): number {
+    return this.reduce(0, (acc, _unused) => acc + 1)
+  }
+
+  toString(this: List<A>): string {
+    return this.reduce('List( ', (acc, item) => `${acc}${item} `) + ')'
   }
 }
 
-export type List<A> = Cons<A> | NilClass
+class Nil<A> extends BaseList<A> {
+  public readonly kind: 'nil' = 'nil'
+
+  readonly isEmpty = (): boolean => true
+}
+
+export const nil = new Nil<never>()
+
+export class Cons<A> extends BaseList<A> {
+  public readonly kind: 'cons' = 'cons'
+
+  constructor(public readonly head: A, public readonly tail: List<A>) {
+    super()
+  }
+
+  readonly isEmpty = (): boolean => false
+}
+
+export type List<A> = Cons<A> | Nil<A>
